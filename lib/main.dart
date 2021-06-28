0d49bddd426936a1fcc0952d115191fbdc06ca70
danZhur <gre4kaDISCOUNT@gmail.com>
2021-06-28 11:02:46 +0900

    feat: added logger

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:cook_cook/app/presentation/app.dart';
+import 'package:cook_cook/core/services/logger.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await LoggerService().init();
   runApp(App());
 }
