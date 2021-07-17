efdb8d40839a67ad0d6ecd41e0657efec64696e0
danZhur <gre4kaDISCOUNT@gmail.com>
2021-07-17 18:23:53 +0900

    faeat: added data source

---
+++ lib/features/recipes/data/datasources/recipes_datasource.dart
@@ -0,0 +1,41 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cook_cook/core/errors/error_exceprions.dart';
+import 'package:cook_cook/features/recipes/data/models/recipe_model.dart';
+
+abstract class RecipesDatasource {
+  Future<List<RecipeModel>> getRecipes();
+  Future<void> addRecipe(RecipeModel recipe);
+}
+
+class RecipesDatasourceImpl implements RecipesDatasource {
+  static const String _endPoint = 'recipes';
+
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  @override
+  Future<List<RecipeModel>> getRecipes() async {
+    try {
+      final response = await firestore.collection(_endPoint).get();
+
+      return List<RecipeModel>.from(
+        response.docs.map((e) => RecipeModel.fromJson(e.data(), e.id)),
+      );
+    } on FirebaseException catch (e) {
+      throw SimpleException(e.toString());
+    } catch (e) {
+      throw SimpleException(e.toString());
+    }
+  }
+
+  @override
+  Future<void> addRecipe(RecipeModel recipe) async {
+    try {
+      final collection = firestore.collection(_endPoint);
+      collection.add(recipe.toJson());
+    } on FirebaseException catch (e) {
+      throw SimpleException(e.toString());
+    } catch (e) {
+      throw SimpleException(e.toString());
+    }
+  }
+}
